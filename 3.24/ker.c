8fcf78cfddc17fd473e7b3dd364423b7f9b47fe6
jijorbq <86359005@qq.com>
2019-03-26 19:02:52 +0800

    constitute of expr3

--- 3.24/ker.c
+++ 3.24/ker.c
@@ -1,13 +1,43 @@
 
-char s[N], t[N];
-void ClearScreen();  // 
+extern void ClearScreen();  // 
+extern void putchar(char c, int r, int c);  // used for 
+extern char getchar();
+extern void puts(char *s, int len, int r, int c); // print a string at the (r,c), used for terminal 
+extern char *readItem(int k);
+extern void Scrolldown();
 
-void puts(char *s, int len, int r, int c); // print a string at the (r,c), used for terminal 
-void putchar(char c, int r, int c);  // used for 
+char s[N], t[N], parm[10][20]; // common used order , parameters
 
-void shell();
+int strnlen(char *s,int n){
+	int len=0;
+	while ( s[len] && len<n)++len;
+	return len;
+}
+void shell(){
+	while ( true){
+		if( r==79) Scrolldown();
+		++r;
+		p= "fuchang@1038 $\0";
+		int len =strnlen(p, MAXLEN);
+		puts(p, len, r,c);
+		Setcursor(r,c+=len);  // ignore the case that r >= 80;
+		memset(s, 0, sizeof(s));len=0;
 
+		// input the order....
+		for (char ch ; (ch=getchar())!= 10; ){
+			if ( len =='' ){
+				if ( len ) putchar(' ', r,--c), s[--len] = 0;
+			}else{
+				putchar(ch, r,c++);s[len++] = ch;
+			}
+		}
+		
+		//parser the order...
+		
+	}
 
-char getchar();
-
+}
 void dir();
+
+
+// and rest of 10000
--- 3.24/loader.asm
+++ 3.24/loader.asm
@@ -1,4 +1,62 @@
+table_head	equ = 0x7e00;table head of the documents 
+[section .load , vstart]
+_start:
 
-[section .table] ; document table
+times 510-($-$$) db 
+db 0x55 , 0xaa
 
-extern table_head	;table head of the documents 
+[section .table, vstart=0x7e00] ; document table
+Itemeach equ 16
+item0:
+	db "kernel",0x0,0x0
+	dw 2
+	dw LengthOfKernel
+	dw CreattimeOfKernel
+	dw 0x0 ; exe
+item1:
+	db "prog1", 0x0,0x0,0x0
+	dw 0x2
+	dw LengthOfProg1
+	dw CreattimeOfProg1
+	dw 0x0 ; exe
+item2:
+	db "prog2", 0x0,0x0,0x0
+	dw 0x3
+	dw LengthOfProg2
+	dw CreattimeOfProg2
+	dw 0x0 ; exe
+item3:
+	db "prog3", 0x0,0x0,0x0
+	dw 0x4
+	dw LengthOfProg3
+	dw CreattimeOfProg3
+	dw 0x0 ; exe
+item4:
+	db "init.cmd"
+	dw 0x5
+	dw LengthOfcmd
+	dw CreattimeOfcmd
+	dw 0x1	; cmd
+
+times 512-($-$$) db 0
+
+[section .kernel, vstart=]
+	mov ax,0x0003  ;03H: 80×25 16色 文本
+	int 0x10
+putchar:
+	;;
+
+getchar:
+	;;
+
+puts:
+	;;
+
+ClearScreen:
+	;;
+
+readItem:
+	;;
+
+ScrollDown:
+; and rest of 10.....
