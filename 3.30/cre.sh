cf669aa48ba796a9bc1bd57c432293e74ed6fd51
jijorbq <86359005@qq.com>
2019-04-09 20:03:06 +0800

    fail exe since c14

--- 3.30/cre.sh
+++ 3.30/cre.sh
@@ -1,5 +1,25 @@
+fil=h.img
 make
-len=$[1024-$(stat -c %s "fd0")]
+cat cmbr.bin > h.img # clear and cat in
+
+cat ccore.bin >> h.img 
+len=$[50*512-$(stat -c %s "$fil")]
 echo len=$len
-dd if=/dev/zero count=$len bs=1 | cat>> fd0
-echo -n -e "\x55\xaa" >> fd0
+dd if=/dev/zero count=$len bs=1 | cat>> $fil
+
+cat c1.bin >> h.img
+len=$[100*512-$(stat -c %s "$fil")]
+echo len=$len
+dd if=/dev/zero count=$len bs=1 | cat>> $fil
+
+cat diskdata.txt >> h.img
+len=$[101*512-$(stat -c %s "$fil")]
+echo len=$len
+dd if=/dev/zero count=$len bs=1 | cat>> $fil
+
+# cat c2.bin >> h.img
+# len=$[18*512-$(stat -c %s "$fil")]
+# echo len=$len
+# dd if=/dev/zero count=$len bs=1 | cat>> $fil
+
+#echo -n -e "\x55\xaa" >> $fil
